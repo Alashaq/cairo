a45ebd8bab206e93802342911cdd84ca39bff620
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-22 00:20:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,6 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '3'
-client.on('ready', () => {
-  console.log('======================================')
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('')
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log('=======================================')
-});
-
-if (command == "say") {
-let rank = message.guild.member(message.author).roles.find('name', '.');
-if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
 var ServerID = "494284550789136384"; //اي دي السيرفر
 var ChannelID = "494284550789136386";// اي دي الروم
-
-
+const prefix = '3'
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -46,4 +25,5 @@ function timerFunc() {
 }
 
 var timer = setTimeout(timerFunc, 1000);
+
 client.login('NDk1Njc1OTI2MDg5MTcwOTU0.DpFkwg.dfREKm68kNJyaF0uCmZ3i2cvbbE');
