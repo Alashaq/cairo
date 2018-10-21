775d45551150f4b5d89182c3c450f12ceb917755
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-22 00:21:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,14 @@ function timerFunc() {
     });
 }
 
+if (command == "say") {
+let rank = message.guild.member(message.author).roles.find('name', '.');
+if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
 var timer = setTimeout(timerFunc, 1000);
 
 client.login('NDk1Njc1OTI2MDg5MTcwOTU0.DpFkwg.dfREKm68kNJyaF0uCmZ3i2cvbbE');
